10694c923e8fcc9fda4f0f5f38bbb113c99217e9
Noel Codes <no3lcodes@gmail.com>
2022-01-06 16:25:54 -0800

    .

--- components/header/header.component.jsx
+++ components/header/header.component.jsx
@@ -20,8 +20,8 @@ const HeaderComponent = () => (
       alt="Noel Covarrubias aka Noel Codes"
     />
     <HeaderTextContainer>
-      {```Hey, I'm Noel, I'm a self-taught software engineer. Besides programming, which is what I do almost all day I also enjoy
-      to read, work out, play video games and to learn about other topics.```}
+      Hey, I'm Noel, I'm a self-taught software engineer. Besides programming, which is what I do almost all day I also enjoy
+      to read, work out, play video games and to learn about other topics.
     </HeaderTextContainer>
     <Link href="/contact">
       <a>
