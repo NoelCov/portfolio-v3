fce4c1c826124a3aedf07b2a43a3944c58201ce0
Noel <no3lcodes@gmail.com>
2023-04-14 01:50:11 -0700

    Added image to header. Fixed path in tsconfig

--- components/layout/layout.component.tsx
+++ components/layout/layout.component.tsx
@@ -6,7 +6,7 @@ interface layoutProps {
 }
 
 const LayoutComponent = ({ children }: layoutProps): JSX.Element => (
-    <div className="w-full py-12 px-6 md:px-20 md:py-16 lg:px-32 lg:py-24 xl:px-96 xl:py-48 bg-backgroundColor flex flex-col items-center">
+    <div className="w-full py-12 px-6 md:px-20 md:py-16 lg:px-32 lg:py-24 xl:px-96 xl:py-44 bg-backgroundColor flex flex-col items-center">
         {children}
     </div>
 );
