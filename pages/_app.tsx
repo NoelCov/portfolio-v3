3a1cfa817152ea3882823c527aaf2f67ea3117a8
Noel <no3lcodes@gmail.com>
2023-05-09 20:44:28 -0700

    Added vercel analytics to my project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,20 @@
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 import Navbar from "../components/navbar/navbar.component";
 import Footer from "../components/footer/footer.component";
 
 import { theme } from "../utils/mui-theme";
+import { ThemeProvider } from "@mui/material";
 
 import "../styles/global.css";
-import { ThemeProvider } from "@mui/material";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
     <ThemeProvider theme={theme}>
         <Navbar />
         <Component {...pageProps} />
         <Footer />
+        <Analytics />
     </ThemeProvider>
 );
 
